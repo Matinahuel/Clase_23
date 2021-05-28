85d4c39be55904a44aeb65eee95ac8bbd4f7c10c
Matinahuel <matiasnahuel2396@gmail.com>
2021-05-28 15:59:42 -0300

    agregue rutas: contacto, producto

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.listen(port, () => console.log('servidor corriendo en el puerto ' + port));
+
+app.get('/',(req, res) => {
+    res.send('home');
+});
+
+app.get('/contactos',(req,res)=> {
+    res.send('Pagina de contactos')
+});
+app.get('/productos',(req,res)=> {
+    res.send('Pagina de productos')
+});
